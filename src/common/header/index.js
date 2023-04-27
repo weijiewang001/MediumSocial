3f6e5d777ad3006af0639042eb992358adf4b51a
wayne <weijiewang001@gmail.com>
2023-04-27 21:58:22 +1000

    optimize reducer

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -24,7 +24,8 @@ import
 class Header extends Component {
 
   getListArea() {
-    if (this.props.focused) {
+    const { focused, list } = this.props;
+    if (focused) {
       return (
         <SearchInfo>
           <SearchInfoTitle>
@@ -33,7 +34,7 @@ class Header extends Component {
           </SearchInfoTitle>
           <SearchInfoList>
             {
-              this.props.list.map((item) => {
+              list.map((item) => {
                 return <SearchInfoItem key={item}>{item}</SearchInfoItem>
               })
             }
@@ -46,6 +47,7 @@ class Header extends Component {
   }
 
   render() {
+    const { focused, handleInputFocus, handleInputBlur  } = this.props;
     return (
       <HeaderWrapper className='dell'>
         <Logo>
@@ -61,17 +63,17 @@ class Header extends Component {
           </NavItem>
           <SearchWrapper>
             <CSSTransition
-              in={this.props.focused}
+              in={focused}
               timeout={200}
               classNames="slide"
             >
               <NavSearch
-                className={this.props.focused ? 'focused' : ''}
-                onFocus={this.props.handleInputFocus}
-                onBlur={this.props.handleInputBlur}
+                className={focused ? 'focused' : ''}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
               ></NavSearch>
             </CSSTransition>
-            <span className={this.props.focused ? 'focused iconfont' : 'iconfont'}>
+            <span className={focused ? 'focused iconfont' : 'iconfont'}>
               &#xe662;
             </span>
             {this.getListArea()}
