45ac98ae9eb8fa530c687657d67624dd21085ec3
wayne <weijiewang001@gmail.com>
2023-04-30 21:38:57 +1000

    add switch function

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -24,20 +24,28 @@ import
 class Header extends Component {
 
   getListArea() {
-    const { focused, list } = this.props;
-    if (focused) {
+    const { focused, list, page, mouseIn, totalPage, handleMouseEnter, handleMouseLeave,handleChangePage } = this.props;
+    const newList = list.toJS();
+    const pageList = [];
+    if (newList.length) {
+      for (let i = (page - 1) * 7; i < page * 7; i++) {
+        // console.log(newList[i])
+        pageList.push(
+          <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+        )
+      }
+    }
+    if (focused || mouseIn) {
       return (
-        <SearchInfo>
+        <SearchInfo 
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}>
           <SearchInfoTitle>
             Top search
-            <SearchInfoSwitch>Switch</SearchInfoSwitch>
+            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage)}>Switch</SearchInfoSwitch>
           </SearchInfoTitle>
           <SearchInfoList>
-            {
-              list.map((item) => {
-                return <SearchInfoItem key={item}>{item}</SearchInfoItem>
-              })
-            }
+            {pageList}
           </SearchInfoList>
         </SearchInfo>
       )
@@ -99,20 +107,38 @@ class Header extends Component {
 const mapStateToProps = (state) => {
   return {
     focused: state.getIn(['header', 'focused']),
-    list: state.getIn(['header', 'list'])
+    list: state.getIn(['header', 'list']),
+    page: state.getIn(['header', 'page']),
+    mouseIn: state.getIn(['header', 'mouseIn']),
+    totalPage: state.getIn(['header', 'totalPage'])
     // 等价于state.get('header').get('focused')
   }
 }
 
 const mapDispathToProps = (dispatch) => {
   return {
-    handleInputFocus(){
+    handleInputFocus() {
       dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus());
     },
-    handleInputBlur(){
+    handleInputBlur() {
       dispatch(actionCreators.searchBlur());
-    }
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseEnter());
+    },
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave());
+    },
+    handleChangePage(page, totalPage) {
+      if (page < totalPage) {
+        dispatch(actionCreators.changePage(page + 1));
+      }else {
+        dispatch(actionCreators.changePage(1));
+      }
+      // console.log(page, totalPage);
+      // dispatch(actionCreators.changePage());
+    },
   }
 }
 
--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -10,9 +10,23 @@ export const searchBlur = () => ({
   type: constants.SEARCH_BLUR
 })
 
+export const mouseEnter = () => ({
+  type: constants.MOUSE_ENTER
+})
+
+export const mouseLeave = () => ({
+  type: constants.MOUSE_LEAVE
+})
+
 const changeList = (data) => ({
   type: constants.CHANGE_LIST,
-  data: fromJS(data)
+  data: fromJS(data),
+  totalPage: Math.ceil(data.length / 7)
+})
+
+export const changePage = (page) => ({
+  type: constants.CHANGE_PAGE,
+  page
 })
 
 export const getList = () => {
